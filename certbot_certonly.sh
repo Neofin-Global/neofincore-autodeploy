5b40a36d30c67fec6c38c52f37966207a31746af
Anton <pythonengineer@omnicampus.io>
2022-06-14 18:16:30 +0300

    Update script

--- certbot_certonly.sh
+++ certbot_certonly.sh
@@ -7,15 +7,8 @@ LOG_FILE="/var/log/certbot_certonly.log";
 exec > >(while read -r line; do printf '[%s] %s\n' "$(date --rfc-3339=seconds)" "$line" | tee -a $LOG_FILE; done);
 exec 2> >(while read -r line; do printf '[%s] %s\n' "$(date --rfc-3339=seconds)" "$line" | tee -a $LOG_FILE; done >&2);
 
-export PARENT_HOST=$PARENT_HOST
+source /var/site/neofincore-autodeploy/.env
 export POSTGRES_HOST=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql)
-export POSTGRES_PORT=$POSTGRES_PORT
-export POSTGRES_DB=$POSTGRES_DB
-export POSTGRES_USER=$POSTGRES_USER
-export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-export DOMAIN_OWNER_EMAIL=$DOMAIN_OWNER_EMAIL
-
-POSTGRES_HOST=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql)
 
 domains_arr=$(python3 /var/site/neofincore-autodeploy/get_merchant_domains.py --domain ${PARENT_HOST} --db_host ${POSTGRES_HOST} --db_port ${POSTGRES_PORT} --db_name ${POSTGRES_DB} --db_user ${POSTGRES_USER} --db_pass ${POSTGRES_PASSWORD});
 echo "NEW merchant domains:";
