235522a2c19ae73e7eb298146284a1a2de135a4c
Anton <pythonengineer@omnicampus.io>
2022-06-15 09:47:07 +0300

    Update script

--- certbot_certonly.sh
+++ certbot_certonly.sh
@@ -11,10 +11,13 @@ source /var/site/neofincore-autodeploy/.env
 export POSTGRES_HOST=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql)
 
 domains_arr=$(python3 /var/site/neofincore-autodeploy/get_merchant_domains.py --domain ${PARENT_HOST} --db_host ${POSTGRES_HOST} --db_port ${POSTGRES_PORT} --db_name ${POSTGRES_DB} --db_user ${POSTGRES_USER} --db_pass ${POSTGRES_PASSWORD});
-echo "NEW merchant domains:";
-echo $domains_arr
+
+if [[ -n "$domains_arr" ]]; then
+  echo "NEW merchant domains:";
+  echo $domains_arr
 
 
-for domain in $domains_arr; do
-  docker compose -f docker-compose.yml run certbot-digitalocean-prod certonly --webroot -w /var/www/certbot --force-renewal --email ${DOMAIN_OWNER_EMAIL} -d $domain --agree-tos
-done
+  for domain in $domains_arr; do
+    docker compose -f docker-compose.yml run certbot-digitalocean-prod certonly --webroot -w /var/www/certbot --force-renewal --email ${DOMAIN_OWNER_EMAIL} -d $domain --agree-tos
+  done
+fi
