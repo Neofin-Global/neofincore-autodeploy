7cc33a77c610deaab150a0153a15337208561620
Anton <pythonengineer@omnicampus.io>
2022-07-12 13:43:33 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py compilemessages
 python manage.py createsuperuser --noinput --email ${DJANGO_SUPERUSER_EMAIL}
+python manage.py loaddata indicator_category indicator
 gunicorn --config gunicorn-cfg.py core.wsgi
