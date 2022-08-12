a20a3e4fa67239a848dbc04b2a46112120dc9b59
Anton <pythonengineer@omnicampus.io>
2022-08-12 10:29:27 +0300

    Update check_release.sh

--- check_release.sh
+++ check_release.sh
@@ -13,29 +13,33 @@ source /var/site/neofincore-autodeploy/.env
 # Get latest release version from Control Panel
 NEW_VERSION=$(curl -s "https://${CONTROL_PANEL_HOST}/api/v1/releases/latest/" --header "Authorization: Service-token ${CONTROL_PANEL_SERVICE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['stripped_tag'])")
 
-echo ${APPLICATION_VERSION}
-echo ${NEW_VERSION}
+AUTOUPDATE_ENABLED=$(curl -s "https://${CONTROL_PANEL_HOST}/api/v1/projects/${PROJECT_SERVICE_TOKEN}/upgrades/" --header "Authorization: Service-token ${CONTROL_PANEL_SERVICE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['active_autoupdate'])")
 
-if [[ "${APPLICATION_VERSION}" == "$NEW_VERSION" ]]; then
-  echo "Versions equal";
-else
-  echo "Versions NOT equal";
+echo "Current version: ${APPLICATION_VERSION}; New version: ${NEW_VERSION}; Autoupdate enabled: ${AUTOUPDATE_ENABLED}";
 
-  # Save current version
-  PREV_VERSION=${APPLICATION_VERSION}
+if [[ "${AUTOUPDATE_ENABLED}" == "True" ]]; then
 
-  # Set new verion to ENV
-  sed -i "s/APPLICATION_VERSION=${APPLICATION_VERSION}/APPLICATION_VERSION=$NEW_VERSION/gI" /var/site/neofincore-autodeploy/.env
-  # Activate ENV
-  source /var/site/neofincore-autodeploy/.env
+  if [[ "${APPLICATION_VERSION}" == "$NEW_VERSION" ]]; then
+    echo "Versions equal";
+  else
+    echo "Versions NOT equal";
 
-  # Update containers
-  docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml up --quiet-pull --build --no-start
-  docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml up -d --quiet-pull
-  # Remove old images
-  docker image prune -a -f
+    # Save current version
+    PREV_VERSION=${APPLICATION_VERSION}
 
-  # Send notification about upgrade to Control Panel (neo-fin.com)
-  curl --location --request POST "https://${CONTROL_PANEL_HOST}/api/v1/projects/${PROJECT_SERVICE_TOKEN}/upgrade/notification/" --header "Authorization: Service-token ${CONTROL_PANEL_SERVICE_TOKEN}" --header 'Content-Type: application/json' --data-raw "{\"previous_version\": \"${PREV_VERSION}\", \"new_version\": \"${NEW_VERSION}\"}"
+    # Set new verion to ENV
+    sed -i "s/APPLICATION_VERSION=${APPLICATION_VERSION}/APPLICATION_VERSION=$NEW_VERSION/gI" /var/site/neofincore-autodeploy/.env
+    # Activate ENV
+    source /var/site/neofincore-autodeploy/.env
 
+    # Update containers
+    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml up --quiet-pull --build --no-start
+    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml up -d --quiet-pull
+    # Remove old images
+    docker image prune -a -f
+
+    # Send notification about upgrade to Control Panel (neo-fin.com)
+    curl --location --request POST "https://${CONTROL_PANEL_HOST}/api/v1/projects/${PROJECT_SERVICE_TOKEN}/upgrades/notification/" --header "Authorization: Service-token ${CONTROL_PANEL_SERVICE_TOKEN}" --header 'Content-Type: application/json' --data-raw "{\"previous_version\": \"${PREV_VERSION}\", \"new_version\": \"${NEW_VERSION}\"}"
+
+  fi
 fi
