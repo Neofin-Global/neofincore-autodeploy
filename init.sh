e53838ed0c43f8baf2fe3a58284618152fff3d0c
Anton <pythonengineer@omnicampus.io>
2022-12-08 11:16:25 +0200

    Update django start up script

--- init.sh
+++ init.sh
@@ -7,20 +7,7 @@ python manage.py initial_token_setup
 
 python manage.py create_superadmin --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL}
 
-indicator_category_count=$(echo "from apps.decision_engine.models.indicator import IndicatorCategory; print(IndicatorCategory.objects.all().count())" | ./manage.py shell);
-if (( $indicator_category_count < 1 )); then
-  echo "Load indicator categories";
-  python manage.py loaddata indicator_category 
-else
-  echo "Predefined indicator categories already exists";
-fi
-
-indicators_count=$(echo "from apps.decision_engine.models.indicator import Indicator; print(Indicator.objects.all().count())" | ./manage.py shell);
-if (( $indicators_count < 1 )); then
-  echo "Load indicators";
-  python manage.py loaddata indicator 
-else
-  echo "Predefined indicators already exists";
-fi
+python3 manage.py loaddata indicator_category
+python3 manage.py load_decision_engine_indicators
 
 gunicorn --config gunicorn-cfg.py core.wsgi
