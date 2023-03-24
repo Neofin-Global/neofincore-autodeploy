59d9ddd01fb107b63ef6f04725091533204e57d7
Anton <pythonengineer@omnicampus.io>
2023-03-24 15:01:39 +0200

    T-657: improve autoupdating scripts and compose

--- check_release.sh
+++ check_release.sh
@@ -20,74 +20,67 @@ if [[ "${AUTOUPDATE_ENABLED}" == "True" ]]; then
     # update stage to latest
     echo "Update stage instance";
 
+
     # Get digest for django container
-    # sha256:1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-    django_sha=$(docker inspect --format='{{index .Image}}' django)
+    # sha256:1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82 or set empty variable if django container not found
+    django_sha=$(sudo docker inspect --format='{{index .Image}}' django) || django_sha=
+    echo "Variable django_sha=${django_sha}"
+    # Get django container hash
     # 1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
     django_sha_clear=$(echo "$django_sha" | sed -r 's/(sha256:)//g')
-    # echo "Django image digest ${django_sha_clear}"
+    echo "Variable django_sha_clear=${django_sha_clear}"
+    # Get django container image name
+    # index.docker.io/phonxis/neofincore_django_master:latest or set empty variable if django container not found
+    django_image=$(sudo docker inspect --format='{{index .Config.Image}}' django) || django_image=
+    echo "Variable django_image=${django_image}"
 
-    # index.docker.io/phonxis/neofincore_django_master:latest
-    django_image=$(docker inspect --format='{{index .Config.Image}}' django)
-    # echo "Django image ${django_image}"
 
-    # phonxis/neofincore_django_master@sha256:4e2256e65c04778f16221ea85b249859400863a9d70fbe11962e43c69d371168
-    repo_digest=$(docker inspect --format='{{index .RepoDigests 0}}' $django_sha_clear)
-    echo "Current image digest ${repo_digest}"
-
-    # sha256:4e2256e65c04778f16221ea85b249859400863a9d70fbe11962e43c69d371168
-    image_digest=$(docker manifest inspect $django_image -v | python3 -c "import sys, json; print(json.load(sys.stdin)['Descriptor']['digest'])")
-    echo "Newest image digest ${image_digest}"
-
-    if [[ $repo_digest == *"$image_digest"* ]];
-    then
-        echo "Stage instance up to date"
+    if [[ -z "$django_sha_clear" ]]; then
+      # if django_sha_clear variable is empty then set repo_digest variable as empty
+      repo_digest=
     else
+      # phonxis/neofincore_django_master@sha256:4e2256e65c04778f16221ea85b249859400863a9d70fbe11962e43c69d371168
+      repo_digest=$(sudo docker inspect --format='{{index .RepoDigests 0}}' $django_sha_clear) || repo_digest=
+    fi
+    echo "Variable repo_digest=${repo_digest}";
+
+
+    if [[ -z "$django_image" ]]; then
+      # if django_image variable is empty then set image_digest variable as empty
+      image_digest=
+    else
+      # sha256:4e2256e65c04778f16221ea85b249859400863a9d70fbe11962e43c69d371168
+      image_digest=$(sudo docker manifest inspect $django_image -v | python3 -c "import sys, json; print(json.load(sys.stdin)['Descriptor']['digest'])")
+    fi
+    echo "Variable image_digest=${image_digest}"
+
+
+    # Check variables are empty or not
+    if [[ -n "$repo_digest" && -n "$image_digest" ]]; then
+      echo "Any of digests is not empty. Check digests equalness."
+      if [[ $repo_digest == *"$image_digest"* ]]; then
+        echo "Stage instance up to date"
+      else
         echo "Stage instance updating..."
-
-        # Get digest for celerybeat container
-        # sha256:1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-        celerybeat_sha=$(docker inspect --format='{{index .Image}}' celerybeat)
-        # 1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-        celerybeat_sha_clear=$(echo "$celerybeat_sha" | sed -r 's/(sha256:)//g')
-        # echo "Celerybeat image digest ${celerybeat_sha_clear}"
-
-        # Get digest for celeryworker container
-        # sha256:1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-        celeryworker_sha=$(docker inspect --format='{{index .Image}}' celeryworker)
-        # 1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-        celeryworker_sha_clear=$(echo "$celeryworker_sha" | sed -r 's/(sha256:)//g')
-        # echo "Celeryworker image digest ${celeryworker_sha_clear}"
-
-        # Get digest for flower container
-        # sha256:1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-        flower_sha=$(docker inspect --format='{{index .Image}}' flower)
-        # 1ecf52911a0c63d8c775cc6f0896ecd92fe0e4368623c587596b662335d16e82
-        flower_sha_clear=$(echo "$flower_sha" | sed -r 's/(sha256:)//g')
-        # echo "Flower image digest ${flower_sha_clear}"
 
         NEW_VERSION="latest";
         echo "Current version: ${APPLICATION_VERSION}; New version: ${NEW_VERSION}";
 
-        # Save current version
-        PREV_VERSION=${APPLICATION_VERSION}
-        # Set new verion to ENV
-        sed -i "s/APPLICATION_VERSION=${APPLICATION_VERSION}/APPLICATION_VERSION=$NEW_VERSION/gI" /var/site/neofincore-autodeploy/.env
-        # Activate ENV
-        source /var/site/neofincore-autodeploy/.env
         # Update containers
-        docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage up --quiet-pull --build --no-start
-        docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage down
-        # Remove cocrete images
-        docker image rm ${django_sha_clear} -f
-        docker image rm ${celerybeat_sha_clear} -f
-        docker image rm ${celeryworker_sha_clear} -f
-        docker image rm ${flower_sha_clear} -f
-
-        docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage up -d --quiet-pull
+        docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} pull -q
+        docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} down
+        docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} up -d
         # Remove old images
         docker image prune -a -f
         # Send notification about upgrade to Control Panel (neo-fin.com)
+        curl --location --request POST "https://${CONTROL_PANEL_HOST}/api/v1/projects/${PROJECT_UID}/upgrades/notification/" --header "Authorization: Service-token ${CONTROL_PANEL_SERVICE_TOKEN}" --header 'Content-Type: application/json' --data-raw "{\"previous_version\": \"${PREV_VERSION}\", \"new_version\": \"${NEW_VERSION}\", \"environment\": \"stage\"}"
+      fi
+    else
+      echo "Both digests are empty."
+      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} down
+      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} pull -q
+      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} up -d
+      # Send notification about upgrade to Control Panel (neo-fin.com)
         curl --location --request POST "https://${CONTROL_PANEL_HOST}/api/v1/projects/${PROJECT_UID}/upgrades/notification/" --header "Authorization: Service-token ${CONTROL_PANEL_SERVICE_TOKEN}" --header 'Content-Type: application/json' --data-raw "{\"previous_version\": \"${PREV_VERSION}\", \"new_version\": \"${NEW_VERSION}\", \"environment\": \"stage\"}"
     fi
 
@@ -113,8 +106,9 @@ if [[ "${AUTOUPDATE_ENABLED}" == "True" ]]; then
       source /var/site/neofincore-autodeploy/.env
 
       # Update containers
-      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production up --quiet-pull --build --no-start
-      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production up -d --quiet-pull
+      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production --profile ${PROVIDER_NAME} down
+      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production --profile ${PROVIDER_NAME} pull -q
+      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production --profile ${PROVIDER_NAME} up -d
       # Remove old images
       docker image prune -a -f
 
