130ea5fdd2356d76973a44af9f89125861f0be7b
Anton <pythonengineer@omnicampus.io>
2023-03-27 17:43:47 +0300

    T-573: replace tabs with spaces

--- certbot_renew.sh
+++ certbot_renew.sh
@@ -12,22 +12,22 @@ exec 2> >(while read -r line; do printf '[%s] %s\n' "$(date --rfc-3339=seconds)"
 source /var/site/neofincore-autodeploy/.env
 
 if [[ "${PROVIDER_NAME}" == "digitalocean" ]]; then
-	CERTBOT_CONTAINER="certbot-digitalocean-stage";
+  CERTBOT_CONTAINER="certbot-digitalocean-stage";
 elif [[ "${PROVIDER_NAME}" == "ec2" ]]; then
-	CERTBOT_CONTAINER="certbot-aws-stage";
+  CERTBOT_CONTAINER="certbot-aws-stage";
 elif [[ "${PROVIDER_NAME}" == "azure" ]]; then
-	CERTBOT_CONTAINER="certbot-azure-stage";
+  CERTBOT_CONTAINER="certbot-azure-stage";
 elif [[ "${PROVIDER_NAME}" == "gce" ]]; then
-	CERTBOT_CONTAINER="certbot-google-stage";
+  CERTBOT_CONTAINER="certbot-google-stage";
 fi
 
 if [[ -n "$CERTBOT_CONTAINER" ]]; then
-	if [[ "${PROJECT_ENVIRONMENT}" == "stage" ]]; then
-		docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage run ${CERTBOT_CONTAINER} renew --quiet
-	    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} restart nginx
-	else
-		docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production run certbot-prod renew --quiet
-	    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production --profile ${PROVIDER_NAME} restart nginx-prod
-	fi
+  if [[ "${PROJECT_ENVIRONMENT}" == "stage" ]]; then
+    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage run ${CERTBOT_CONTAINER} renew --quiet
+    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} restart nginx
+  else
+    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production run certbot-prod renew --quiet
+    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production --profile ${PROVIDER_NAME} restart nginx-prod
+  fi
 fi
 
