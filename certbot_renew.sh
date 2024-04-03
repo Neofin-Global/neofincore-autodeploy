662a1bd112366fd351a5e1e7eab9be7ac08e4cff
alex <sashalb93@gmail.com>
2024-04-03 17:42:24 +0300

    feature: support local.docker-compose.yml

--- certbot_renew.sh
+++ certbot_renew.sh
@@ -21,17 +21,22 @@ elif [[ "${PROVIDER_NAME}" == "gce" ]]; then
   CERTBOT_CONTAINER="certbot-google-stage";
 fi
 
+# Check if local.docker-compose.yml exists, use it, otherwise fallback to docker-compose.yml
+COMPOSE_FILE="docker-compose.yml"
+if [ -f "/var/site/neofincore-autodeploy/local.docker-compose.yml" ]; then
+  COMPOSE_FILE="local.docker-compose.yml"
+fi
+
 if [[ -n "$CERTBOT_CONTAINER" ]]; then
   if [[ "${PROJECT_ENVIRONMENT}" == "stage" ]]; then
     if [[ "${PROVIDER_NAME}" == "azure" ]]; then
-      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage run ${CERTBOT_CONTAINER} certbot renew --quiet
+      docker compose -f "/var/site/neofincore-autodeploy/$COMPOSE_FILE" --profile stage run ${CERTBOT_CONTAINER} certbot renew --quiet
     else
-      docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage run ${CERTBOT_CONTAINER} renew --quiet
+      docker compose -f "/var/site/neofincore-autodeploy/$COMPOSE_FILE" --profile stage run ${CERTBOT_CONTAINER} renew --quiet
     fi
-    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile stage --profile ${PROVIDER_NAME} restart nginx
+    docker compose -f "/var/site/neofincore-autodeploy/$COMPOSE_FILE" --profile stage --profile ${PROVIDER_NAME} restart nginx
   else
-    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production run certbot-prod renew --quiet
-    docker compose -f /var/site/neofincore-autodeploy/docker-compose.yml --profile production --profile ${PROVIDER_NAME} restart nginx-prod
+    docker compose -f "/var/site/neofincore-autodeploy/$COMPOSE_FILE" --profile production run certbot-prod renew --quiet
+    docker compose -f "/var/site/neofincore-autodeploy/$COMPOSE_FILE" --profile production --profile ${PROVIDER_NAME} restart nginx-prod
   fi
 fi
-
